fb858306febf707b9ca339b667ba41804ac35a34
Jan Szebesta <szebesta.jan@gmail.com>
2024-04-02 13:28:42 +0200

    feat: tip calculator with notes

---
+++ TipCalculator/Notes/DataTypes.py
@@ -0,0 +1,23 @@
+#Data Types
+
+#String
+
+print("Hello"[0]) #[] allows subscripting - pulls first letter from "Hello"
+
+#Integer (Whole numbers/Not integers)
+
+print(123_456_900 + 345)
+
+123_456_900
+
+#Float (Decimal point)
+
+3.14159
+
+#Boolean
+
+True
+False
+
+
+
---
+++ TipCalculator/Notes/TypeErr.py
@@ -0,0 +1,17 @@
+# num_char = len(input("What is your name ?\n"))
+# new_num_char = str(num_char)
+
+# print("Your name is " + new_num_char + " letters long")
+
+# # print(type(num_char)) #type() can be used to see what type of datatype is being used.
+
+a = float(123)
+print(type(a))
+
+print(70 + float("100.5"))
+
+# str()
+# int()
+# float()
+
+#can be used to convert data types to your desired state.
