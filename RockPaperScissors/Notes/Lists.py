104b9c70157247d7e692c1ec9edec8d947a487e8
Jan Szebesta <szebesta.jan@gmail.com>
2024-04-24 13:31:41 +0200

    Rock Paper Scissors

---
+++ RockPaperScissors/Notes/Lists.py
@@ -0,0 +1,29 @@
+#https://docs.python.org/3/tutorial/datastructures.html
+
+# states_of_america = ["Delaware", "Pennsylvania"]
+
+# states_of_america.append("Janland")
+
+# states_of_america.extend(["Testing"])
+
+# print(states_of_america)
+
+# states_of_america.len
+
+# fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+# vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+# dirty_dozen = [fruits, vegetables]
+
+# print(dirty_dozen)
+
+# fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+# fruits[-1] = "Melons"
+# fruits.append("Lemons")
+# print(fruits)
+
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+     
+dirty_dozen = [fruits, vegetables]
+     
+print(dirty_dozen[1][2])
