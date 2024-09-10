65a8900a5ec8b9230cf88003645abd86fb4fc172
Jan Szebesta <szebesta.jan@gmail.com>
2024-09-10 08:57:10 +0200

    tip calculator

---
+++ TipCalculator/methematipcalops.py
@@ -0,0 +1,16 @@
+print(123 + 456)
+print(7 - 3)
+print(9 * 3)
+print(6 / 3)
+print(6 // 3)
+print(2 ** 3)
+
+# PEMDAS
+# ()
+# **
+# * OR /
+# + OR -
+
+print(3 * 3 + 3 / 3 - 3)
+
+# 9 + 1 - 3 = 7.0 because its a float
---
+++ TipCalculator/numbermanipulation.py
@@ -0,0 +1,29 @@
+bmi = 84 / 1.65 ** 2
+print(bmi)
+
+print (int(bmi))
+
+print(round(bmi, 2))
+
+# Assignment Operator
+# +=
+# -=
+
+
+score = 0 
+
+# User scores a point
+
+score += 1
+
+print(score)
+
+# f-strings
+
+score = 0 
+height = 1.8
+is_winning = True
+
+print(f"My height is {height}")
+
+print(6 + 4 / 2 - (1 * 2))
