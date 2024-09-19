1c3ce242deeef8b5f321e9b1944331015f89e7d6
Jan Szebesta <szebesta.jan@gmail.com>
2024-09-19 22:56:37 +0200

    rock paper scissors

---
+++ .notes/008.randommodule.py
@@ -0,0 +1,19 @@
+import random
+
+# random_integer = random.randint(1, 10)
+# print(random_integer)
+
+
+# random_float_0_to_1 = random.random() * 10
+# print(random_float_0_to_1)
+
+# random_float = random.uniform(1, 10)
+# print(random_float)
+
+# Simple logic for heads and tails to understand
+random_integer = random.randint(1, 2)
+
+if random_integer == 1:
+    print("Heads")
+else:
+    print("Tails")
---
+++ .notes/009.lists.py
@@ -0,0 +1,27 @@
+fruits = ["Cherry", "Apple", "Pear"]
+
+fruits[0] = "Lime"
+
+fruits.append("Grape")
+
+fruits.extend(["Pineapple", "Lemon"])
+
+print(fruits)
+
+# Randomization plus lists usecase
+
+import random
+friends = ["Alice", "Bob", "Charlie","David", "Emanuel"]
+
+random_int = random.randint(0, 4)
+
+who_pays = friends[random_int]
+
+print(f"{who_pays} will pay today.")
+
+# Another way to do this:
+
+print(random.choice(friends))
+
+print(len(friends))
+
